073d7f97ab913a0c29bccf13ac0c8eaa80b8df9d
Manuel Leuenberger <leuenberger@inf.unibe.ch>
2020-04-20 15:27:30 +0200

    improve looks

--- index.js
+++ index.js
@@ -29,12 +29,6 @@ const HTML = `<!doctype html>
   			font-family: Helvetica, Arial, sans-serif;
   			color: rgba(0, 127, 255, 0.5);
 		}
-		.meta {
-			position: absolute;
-			top: 0.5em;
-			left: 0.5em;
-  			font-size: 2em;
-		}
 		.overlay {
 			display: grid;
 			position: absolute;
@@ -44,41 +38,94 @@ const HTML = `<!doctype html>
 			height: 100%;
 			cursor: pointer;
 		}
-		.overlay p {
-			margin: auto;
-  			font-size: 15vh;
-  			text-align: center;
+		.overlay > p {
+			margin: 3em auto auto auto;
 			user-select: none;
+			padding-bottom: 3em;
+			text-align: center;
+		}
+		.message {
+  			font-size: 2em;
+		}
+		.label {
+  			position: absolute;
+  			padding: 0.25em;
+		}
+		.temperature {
+			right: 0;
+  			color: rgba(0, 127, 255, 0.5);
+		}
+		.rain {
+			left: 0;
+  			color: rgba(255, 127, 0, 0.5);
+		}
+		.min {
+			bottom: 0;
+		}
+		.max {
+			top: 0;
+		}
+		.time {
+  			position: absolute;
+  			top: 0;
+  			left: 0;
+  			width: 100%;
+  			height: 100%;
+    		background-blend-mode: multiply;
+  			background: rgba(255, 255, 2555, 0.25);
 		}
 	</style>
 	<script>
 		const load = () => {
-			document.querySelector('.overlay p').innerHTML = 'wart hurti...'
+			document.querySelector('.message').innerHTML = 'wart hurti...'
 			navigator.geolocation.getCurrentPosition(e => {
 				window.location.href = '/location?latitude=' + e.coords.latitude + '&longitude=' + e.coords.longitude
 			}, e => {
-				document.querySelector('.overlay p').style.fontSize = '10vh'
-				document.querySelector('.overlay p').innerHTML = 'aktivier doch ortigdienscht ide ischtellige du möff, süch chani o nüt mache'
+				document.querySelector('.message').innerHTML = 'aktivier doch ortigdienscht ide ischtellige du möff, süsch chani o nüt mache: einstellungen > datenschutz > ortungsdienste > safari'
 			})
 		}
 		document.addEventListener('DOMContentLoaded', () => {
-			if (window.location.pathname != '/location') {
-				document.querySelectorAll('.meta, svg').forEach(e => e.style.display = 'none')
+			if (window.location.pathname !== '/location') {
+				document.querySelectorAll('.meta, .label, .time, svg').forEach(e => e.style.display = 'none')
+			} else {
+				let now = new Date()
+				document.querySelector('.clock').innerHTML = now.toLocaleTimeString()
+				document.querySelector('.time').style.width = '' + 100 * (now.getHours() * 60 + now.getMinutes()) / (24 * 60) + '%'
 			}
 			document.querySelector('.overlay').addEventListener('click', load)
 			document.querySelector('.overlay').addEventListener('touchend', load)
+			if (window.location.pathname === '/') {
+				load()
+			}
 		})
 	</script>
 </head>
 <body>
-	<p class="meta">{3} {4}°C {5}%</p>
 	<svg width="100vw" height="100vh" stroke="none" fill="none" viewBox="0 0 100 100" preserveAspectRatio="none">
 		<path d="{1}" fill="rgba(255, 127, 0, 0.5)"></path>
 		<path d="{2}" fill="rgba(0, 127, 255, 0.5)"></path>
-		<circle cx="{6}" cy="{7}" r="2" fill="rgba(0, 127, 255, 0.5)"/>
 	</svg>
+	<div class="time"></div>
+	<p class="label temperature min">0°C</p>
+	<p class="label temperature max">40°C</p>
+	<p class="label rain min">0%</p>
+	<p class="label rain max">100%</p>
 	<div class="overlay">
-		<p>{0}</p>
+		<p>
+			<span class="message">{0}</span>
+			<span class="meta">
+				<br>
+				<br>
+				<span class="clock"></span>
+				<br>
+				{3}
+				<br>
+				<br>
+				max {4}°C
+				<br>
+				max {5}% räge
+			</span>
+		</p>
 	</div>
 </body>
 </html>`
@@ -202,65 +249,74 @@ const d = (points, smoothing = 0.5) => {
 	}
 	return d
 }
-const normalize = (a, h) => a.map((y, i) => {
-	return {
-		x: i * 100 / 8,
-		y: 100 - y * 100 / h
-	}
-})
+const normalize = (a, h) => {
+	a = a.map((y, i) => {
+		return {
+			x: i * 100 / 8 + 100 / 16,
+			y: 100 - y * 100 / h
+		}
+	})
+	a.unshift({
+		x: 0,
+		y: (3 * a[0].y - a[1].y) / 2
+	})
+	a.push({
+		x: 100,
+		y: (3 * a[a.length - 1].y - a[a.length - 2].y) / 2
+	})
+	return a
+}
 
 http.createServer((request, response) => {
-		if (request.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV !== 'development') {
-			response.writeHead(301, {
-				'Location': 'https://' + request.headers['host'] + request.url
-			})
-			response.end()
-			return
-		}
-		if (['/manifest.json', '/icon-192.png', '/icon-512.png'].indexOf(request.url) >= 0) {
-			let p = request.url.substring(1)
-			response.writeHead(200, {
-				'Content-Type': p.endsWith('.json') ? 'application/json' : 'image/png',
-				'Content-Length': fs.statSync(p).size
-			})
-			fs.createReadStream(p).pipe(response)
-			return
-		}
-		if (request.url == '/') {
-			response.statusCode = 200
-			response.end(HTML.format('drück mau hie'))
-			return
-		}
-		if (!request.url.startsWith('/location?latitude')) {
-			response.statusCode = 404
-			response.end(HTML.format('das gits nid'))
-			return
-		}
-		let u = new url.URL('http://' + request.headers['host'] + request.url)
-		let latitude = parseFloat(u.searchParams.get('latitude'))
-		let longitude = parseFloat(u.searchParams.get('longitude'))
-		if (isNaN(latitude) || isNaN(longitude)) {
-			response.statusCode = 200
-			response.end(HTML.format('drück mau hie'))
-			return
-		}
-		getWeatherAuthentication(CH_ID, CH_SECRET)
-			.then(authentication => getWeatherForecast(latitude, longitude, authentication.access_token))
-			.then(forecast => {
-				let location = forecast.info.name.de
-				let temperatures = forecast['24hours'].map(d => parseFloat(d.values[1].ttt))
-				let rains = forecast['24hours'].map(d => parseFloat(d.values[6].pr3))
-				let temperature = Math.max.apply(null, temperatures)
-				let rain = Math.max.apply(null, rains)
-				let message = getMessage(temperature, rain)
-				let dTemperature = `M 0 100 ${d(normalize(temperatures, 50))} L 100 100`
-				let dRain = `M 0 100 ${d(normalize(rains, 100))} L 100 100`
-				let now = new Date()
-				response.statusCode = 200
-				response.end(HTML.format(message, dRain, dTemperature, location, temperature, rain, 100 / 24 * now.getHours(), 100 - 100 / 50 * temperatures[Math.floor(now.getHours() / 3)]))
-			}, data => {
-				response.statusCode = 500
-				response.end(HTML.format('ke ahnig'))
-			})
+	if (request.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV !== 'development') {
+		response.writeHead(301, {
+			'Location': 'https://' + request.headers['host'] + request.url
+		})
+		response.end()
+		return
 	}
-).listen(PORT)
+	if (['/manifest.json', '/icon-192.png', '/icon-512.png'].indexOf(request.url) >= 0) {
+		let p = request.url.substring(1)
+		response.writeHead(200, {
+			'Content-Type': p.endsWith('.json') ? 'application/json' : 'image/png',
+			'Content-Length': fs.statSync(p).size
+		})
+		fs.createReadStream(p).pipe(response)
+		return
+	}
+	if (request.url === '/') {
+		response.statusCode = 200
+		response.end(HTML.format('wart hurti...'))
+		return
+	}
+	if (!request.url.startsWith('/location')) {
+		response.statusCode = 404
+		response.end(HTML.format('das gits nid'))
+		return
+	}
+	let u = new url.URL('http://' + request.headers['host'] + request.url)
+	let latitude = parseFloat(u.searchParams.get('latitude'))
+	let longitude = parseFloat(u.searchParams.get('longitude'))
+	if (isNaN(latitude) || isNaN(longitude)) {
+		response.statusCode = 404
+		response.end(HTML.format('das gits nid'))
+		return
+	}
+	getWeatherAuthentication(CH_ID, CH_SECRET)
+		.then(authentication => getWeatherForecast(latitude, longitude, authentication.access_token))
+		.then(forecast => {
+			let location = forecast.info.name.de
+			let temperatures = forecast['24hours'].map(d => parseFloat(d.values[1].ttt))
+			let rains = forecast['24hours'].map(d => parseFloat(d.values[6].pr3))
+			let temperature = Math.max.apply(null, temperatures)
+			let rain = Math.max.apply(null, rains)
+			let message = getMessage(temperature, rain)
+			let dTemperature = `M 0 100 ${d(normalize(temperatures, 40))} L 100 100`
+			let dRain = `M 0 100 ${d(normalize(rains, 100))} L 100 100`
+			response.statusCode = 200
+			response.end(HTML.format(message, dRain, dTemperature, location, temperature, rain))
+		}, data => {
+			response.statusCode = 500
+			response.end(HTML.format('ke ahnig'))
+		})
+}).listen(PORT)
